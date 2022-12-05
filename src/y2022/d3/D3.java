dc294255363687d0a0e685d749bac3862cb74751
DJ_Laser <devin.t.myers@gmail.com>
2022-12-05 17:47:08 -0500

    start day 3

---
+++ src/y2022/d3/D3.java
@@ -0,0 +1,15 @@
+package y2022.d3;
+
+import util.AOCDay;
+
+public class D3 extends AOCDay {
+    @Override
+    public void challenge1() {
+
+    }
+
+    @Override
+    public void challenge2() {
+
+    }
+}
