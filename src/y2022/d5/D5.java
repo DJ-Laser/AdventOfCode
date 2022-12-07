cf3913ff00fe521acf47dde7fe9b37858c28068c
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2022-12-07 15:05:58 +0000

    Day 5 done

--- src/y2022/d5/D5.java
+++ src/y2022/d5/D5.java
@@ -2,50 +2,116 @@ package y2022.d5;
 
 import util.AOCDay;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
 
 public class D5 extends AOCDay {
     @Override
     public void challenge1() {
-        String input = readInput(true);
+        String input = readInput(false);
         String[] lines = input.split("\r?\n");
-        List<Stack<Character>> containerStacks = new ArrayList<>();
-        List<String> instructions = new ArrayList<>();
-        boolean writeInstructions = false;
+        List<Deque<Character>> containerStacks = new ArrayList<>();
+        List<String> containerLines = new ArrayList<>();
+        boolean doMovements = false;
 
         for (String line:lines) {
-            if(writeInstructions){
-                instructions.add(line);
-                continue;
+            if(doMovements){
+                parseMovement(line, containerStacks, false);
             } else if (line.equals("")) {
-                writeInstructions = true;
-                continue;
-            }
+                doMovements = true;
+            } else if(line.contains("[")){
+                containerLines.add(line);
+            } else {
+                for (int i = 0; i < line.length(); i++) {
+                    if(line.charAt(i) != ' '){
+                        containerStacks.add(new ArrayDeque<Character>());
+                    }
+                }
 
-            if(line.contains("[")){
-                parseContainerLine(line, containerStacks);
+                for (String container : containerLines) {
+                    parseContainerLine(container, containerStacks);
+                }
+            }
+        }
+        for (int i = 0; i < containerStacks.size(); i++) {
+            Deque<Character> stack = containerStacks.get(i);
+            System.out.print(stack.peek());
+        }
+        System.out.println();
+    }
+
+    private void parseMovement(String line, List<Deque<Character>> containerStacks, boolean chal2) {
+        int[] movementParams = line.chars()
+            .filter(i -> Character.isDigit(i))
+            .map(i -> Integer.parseInt("" + (char) i))
+            .toArray();
+        if (movementParams.length  == 4) {
+            movementParams[0] = Integer.parseInt("" + movementParams[0] + movementParams[1]);
+            movementParams[1] = movementParams[2];
+            movementParams[2] = movementParams[3];
+        }
+        Deque<Character> from = containerStacks.get(movementParams[1] - 1);
+        Deque<Character> to = containerStacks.get(movementParams[2] - 1);
+        
+        if (!chal2) {
+            for (int i = 0; i < movementParams[0]; i++) {
+                to.push(from.pop());
+            }
+        } else {
+            Character[] containers = new Character[movementParams[0]];
+            for (int i = 0; i < movementParams[0]; i++) {
+                containers[i] = from.pop();
+            }
+            for (int i = containers.length - 1; i > -1; i--) {
+                to.push(containers[i]);
             }
         }
     }
 
-    private void parseContainerLine(String line, List<Stack<Character>> containerStacks) {
+    private void parseContainerLine(String line, List<Deque<Character>> containerStacks) {
         char[] chars = line.toCharArray();
-        for (int i = 0; i < chars.length; i += 0) {
-            char c = chars[i];
-            if (c == '[') {
-                i++;
-
-            } else {
-                i += 4;
+        for (int i = 0; i < chars.length; i += 4) {
+            int j = i / 4;
+            if (chars[i] == '['){
+                containerStacks.get(j).addLast(chars[i + 1]);
             }
         }
     }
 
     @Override
     public void challenge2() {
+        String input = readInput(false);
+        String[] lines = input.split("\r?\n");
+        List<Deque<Character>> containerStacks = new ArrayList<>();
+        List<String> containerLines = new ArrayList<>();
+        boolean doMovements = false;
 
+        for (String line:lines) {
+            if(doMovements){
+                parseMovement(line, containerStacks, true);
+            } else if (line.equals("")) {
+                doMovements = true;
+            } else if(line.contains("[")){
+                containerLines.add(line);
+            } else {
+                for (int i = 0; i < line.length(); i++) {
+                    if(line.charAt(i) != ' '){
+                        containerStacks.add(new ArrayDeque<Character>());
+                    }
+                }
+
+                for (String container : containerLines) {
+                    parseContainerLine(container, containerStacks);
+                }
+            }
+        }
+        for (int i = 0; i < containerStacks.size(); i++) {
+            Deque<Character> stack = containerStacks.get(i);
+            System.out.print(stack.peek());
+        }
+        System.out.println();
     }
 }
