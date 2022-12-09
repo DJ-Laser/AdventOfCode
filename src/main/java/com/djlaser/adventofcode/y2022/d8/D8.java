2b93988eda68e31bf9abe365400d74a8b3c4b854
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2022-12-09 19:32:20 +0000

    day 8 done

--- src/main/java/com/djlaser/adventofcode/y2022/d8/D8.java
+++ src/main/java/com/djlaser/adventofcode/y2022/d8/D8.java
@@ -1,8 +1,6 @@
 package com.djlaser.adventofcode.y2022.d8;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.djlaser.adventofcode.util.AOCDay;
@@ -23,62 +21,6 @@ public class D8 extends AOCDay {
             this.column = column;
             this.name = name;
         }
-    }
-
-    @Override
-    public void challenge2() {
-        String input = readInput(true);
-        String[] lines = input.split("\r?\n");
-        int[][] trees = new int[lines.length][];
-        int bestScore = 0;
-
-        for (int i = 0; i < lines.length; i++) {
-            trees[i] = lines[i].chars().map(c -> Integer.parseInt("" + (char) c)).toArray();
-        }
-
-        for (int i = 1; i < trees.length - 1; i++) {
-
-            for (int j = 1; j < trees[i].length - 1; j++) {
-                int treeHeight = trees[i][j];
-
-                List<Integer> scores = new ArrayList<>();
-                for (Direction dir : Direction.values()) {
-                    boolean stop = false;
-                    int score = 0;
-
-                    for (int k = 1; ; k++) {
-                        int row = i + dir.row * k;
-                        int column = j + dir.column * k;
-                        
-                        if (row < 0 || row >= trees.length) {
-                            break;
-                        } else if (column < 0 || column >= trees.length) {
-                            break;
-                        }
-
-                        int otherTreeHeight = trees[row][column];
-                        if (treeHeight <= otherTreeHeight) {
-                            scores.add(score);
-                            break;
-                        } else {
-                            score ++;
-                        }
-                    }
-                    
-                    scores.add(score);
-
-                    if (stop) {
-                        break;
-                    }
-                }
-                int myScore = scores.stream().reduce(1, (sub, el) -> sub * el);
-                if (myScore > bestScore) {
-                    bestScore = myScore;
-                }
-            }
-        }
-
-        System.out.println(bestScore);
     }
 
     @Override
@@ -134,4 +76,54 @@ public class D8 extends AOCDay {
         System.out.println(score + trees.length * 4 - 4);
     }
     
+    @Override
+    public void challenge2() {
+        String input = readInput(false);
+        String[] lines = input.split("\r?\n");
+        int[][] trees = new int[lines.length][];
+        int bestScore = 0;
+
+        for (int i = 0; i < lines.length; i++) {
+            trees[i] = lines[i].chars().map(c -> Integer.parseInt("" + (char) c)).toArray();
+        }
+
+        for (int i = 1; i < trees.length - 1; i++) {
+
+            for (int j = 1; j < trees[i].length - 1; j++) {
+                int treeHeight = trees[i][j];
+
+                List<Integer> scores = new ArrayList<>();
+                for (Direction dir : Direction.values()) {
+                    int score = 0;
+
+                    for (int k = 1; ; k++) {
+                        int row = i + dir.row * k;
+                        int column = j + dir.column * k;
+                        
+                        if (row < 0 || row >= trees.length) {
+                            break;
+                        } else if (column < 0 || column >= trees.length) {
+                            break;
+                        }
+
+                        score ++;
+
+                        int otherTreeHeight = trees[row][column];
+                        if (treeHeight <= otherTreeHeight) {
+                            break;
+                        }
+                    }
+                    
+                    scores.add(score);
+                }
+                int myScore = scores.stream().reduce(1, (sub, el) -> sub * el);
+
+                if (myScore > bestScore) {
+                    bestScore = myScore;
+                }
+            }
+        }
+
+        System.out.println(bestScore);
+    }
 }
